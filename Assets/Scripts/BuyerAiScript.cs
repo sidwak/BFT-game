65fdce33d957a0d404f467aeb57b18daeb943b9d
Siddeshwar Wakkalkar <61682424+sidwak@users.noreply.github.com>
2022-01-14 23:07:01 +0530

    Zone 4 added
    starting new type of machine

---
+++ Assets/Scripts/BuyerAiScript.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BuyerAiScript : MonoBehaviour
+{
+    public Vector3 startPos;
+    public Vector3[] racksPos;
+    public Vector3 targetRack;
+
+    public bool isMoving = true;
+    public bool stateIdle = false;
+    public bool stateMovingToRack = false;
+    public bool stateCollectingBoxes = false;
+    public bool stateGoingBack = false;
+    public bool stateResetBox = false;
+
+    private int leastRackBoxCount = 0;
+    private int targetRackid = 0;
+
+    public float startDelay;
+
+    public MoverBoxScript playerBoxScript;
+    public RackBoxHolderScript[] racks;
+
+    private NavMeshAgent navMeshAgent;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        StartCoroutine(StartDelay());
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(startDelay);
+        stateIdle = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Blend", navMeshAgent.velocity.magnitude / navMeshAgent.speed, 0.2f, Time.deltaTime);
+
+        if (stateIdle)
+        {
+            for (int i = 0; i < racks.Length; i++)
+            {
+                if (racks[i].boxCountnum >= leastRackBoxCount && racks[i].gameObject.activeInHierarchy)
+                {
+                    if (!racks[i].isBuyerAiTarget)
+                    {
+                        targetRack= racksPos[i];
+                        leastRackBoxCount = racks[i].boxCountnum;
+                        targetRackid = i;
+                    }
+                }
+            }
+            racks[targetRackid].isBuyerAiTarget = true;
+            leastRackBoxCount = 0;
+            stateMovingToRack = true;
+            stateIdle = false;
+        }
+        if (stateMovingToRack)
+        {
+            navMeshAgent.destination = targetRack;
+            if (Vector3.Distance(transform.position, targetRack) < 0.2f)   //can use stoppingdistance
+            {
+                transform.position = targetRack;
+                stateMovingToRack = false;
+                stateCollectingBoxes = true;
+                isMoving = false;
+            }
+        }
+        if (stateCollectingBoxes)
+        {
+            StartCoroutine(WaitForBoxes());
+            if (playerBoxScript.boxNumber >= 5)
+            {
+                racks[targetRackid].isBuyerAiTarget = false;
+                stateCollectingBoxes = false;
+                stateGoingBack = true;
+                isMoving = true;
+            }
+        }
+        if (stateGoingBack)
+        {
+            navMeshAgent.destination = startPos;
+            if (Vector3.Distance(transform.position, startPos) < 1f)   //can use stoppingdistance
+            {
+                transform.position = startPos;
+                stateGoingBack = false;
+                stateResetBox = true;
+            }
+        }
+        if (stateResetBox)
+        {
+            for (int i = 0; i < playerBoxScript.PlayerBoxesList.Count; i++)
+            {
+                if (playerBoxScript.PlayerBoxesList[i].activeInHierarchy)
+                {
+                    playerBoxScript.PlayerBoxesList[i].SetActive(false);
+                    playerBoxScript.boxNumber--;
+                }
+            }
+            stateResetBox = false;
+            stateIdle = true;
+        }
+
+        IEnumerator WaitForBoxes()
+        {
+            yield return new WaitForSeconds(5f);  //previous
+            if (stateCollectingBoxes)
+            {
+                if (!stateGoingBack)
+                {
+                    stateCollectingBoxes = false;
+                    stateGoingBack = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RackBoxHolderScript.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RackBoxHolderScript : MonoBehaviour
+{
+    public int boxCountnum = 0;
+    public int maxBoxCount;
+
+    public bool isBuyerAiTarget = false;
+    private bool isPlayerInArea = false;
+    private bool isMoverInArea = false;
+
+    public Vector2 offset;
+    public Vector2 canvasScale;
+
+    public GameObject CoinPrefab;
+
+    public PlayerBoxScript playerBoxScript;
+    public CanvasScript canvasScript;
+    private List<GameObject> rackBoxesList = new List<GameObject>();
+    private MoverBoxScript moverBoxScript;
+    private BuyerAiScript buyerAiScript;
+
+    public GameObject RackTextMainObject;
+    public TextMeshProUGUI rackText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Transform child in transform)
+        {
+            rackBoxesList.Add(child.gameObject);
+        }
+        for (int i = 0; i < rackBoxesList.Count; i++)
+        {
+            rackBoxesList[i].SetActive(false);
+        }
+        for (int i = 0; i < boxCountnum; i++)
+        {
+            rackBoxesList[i].SetActive(true);
+        }
+        RackTextMainObject.SetActive(true);
+        canvasScale = new Vector2(1080f / Screen.width, 1920f / Screen.height);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 numPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x + offset.x, transform.position.y + offset.y, transform.position.z));
+        Vector2 Posnum;
+        Posnum.x = numPos.x;
+        Posnum.y = numPos.y;
+        Posnum.x *= canvasScale.x;
+        Posnum.y *= canvasScale.y;
+        //Posnum += offset;
+        RackTextMainObject.GetComponent<RectTransform>().anchoredPosition = Posnum;
+
+        rackText.text = boxCountnum.ToString() + "/" + maxBoxCount.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "BuyerAI")
+        {
+            if (isMoverInArea)
+            {
+                return;
+            }
+            moverBoxScript = other.GetComponentInChildren<MoverBoxScript>();
+            buyerAiScript = other.GetComponent<BuyerAiScript>();
+            isMoverInArea = true;
+            StartCoroutine(MoverStay());
+            return;
+        }
+        isPlayerInArea = true;
+        StartCoroutine(PlayerStay());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isMoverInArea)
+        {
+            isMoverInArea = false;
+        }
+        isPlayerInArea = false;
+    }
+
+    IEnumerator PlayerStay()
+    {
+        yield return new WaitForSeconds(0.075f);
+        if (playerBoxScript.boxNumber == 0 || boxCountnum == maxBoxCount)
+        {
+            yield break;
+        }
+        int curBox = playerBoxScript.boxNumber - 1;
+        canvasScript.totalBoxes--;
+        playerBoxScript.PlayerBoxesList[curBox].SetActive(false);
+        GameObject refBox = playerBoxScript.PlayerBoxesList[curBox];
+        playerBoxScript.boxNumber--;
+        GameObject numBox = Instantiate(playerBoxScript.PlayerBoxesList[0], refBox.transform.position, refBox.transform.rotation);
+        numBox.SetActive(true);
+        //GameObject numCoin = Instantiate(CoinPrefab, numBox.transform.position, CoinPrefab.transform.rotation);
+        //numCoin.GetComponent<CoinAnimationScript>().endTarget = coinEndPostion;
+        numBox.AddComponent<BoxScript>();
+        numBox.GetComponent<BoxScript>().targetobject = rackBoxesList[boxCountnum];
+        numBox.GetComponent<BoxScript>().isTargetset = true;
+        boxCountnum++;
+        if (isPlayerInArea)
+        {
+            StartCoroutine(PlayerStay());
+        }
+    }
+
+    IEnumerator MoverStay()
+    {
+        yield return new WaitForSeconds(0.15f);
+        if (buyerAiScript.isMoving)
+        {
+            StartCoroutine(MoverStay());
+            yield break;
+        }
+        if (moverBoxScript.boxNumber == 5)
+        {
+            yield break;
+        }
+        GameObject refBox = null;
+        int pos = 0;
+        for (int i = rackBoxesList.Count - 1; i >= 0; i--)
+        {          
+            if (rackBoxesList[i].activeInHierarchy)
+            {
+                refBox = rackBoxesList[i];
+                pos = i;
+                break;
+            }
+        }
+        if (refBox == null)
+        {
+            StartCoroutine(MoverStay());
+            yield break;
+        }
+        //GameObject num = Instantiate(activeBoxesList[0], activeBoxesList[activeBoxesList.Count-1].transform.position, activeBoxesList[0].transform.rotation);
+        GameObject num = Instantiate(rackBoxesList[pos], refBox.transform.position, rackBoxesList[pos].transform.rotation);
+        num.transform.localScale = Vector3.one;
+        num.AddComponent<BoxScript>();
+        int numbox = moverBoxScript.boxNumber;
+        num.GetComponent<BoxScript>().targetobject = moverBoxScript.PlayerBoxesList[numbox];
+        num.GetComponent<BoxScript>().isTargetset = true;
+        num.GetComponent<BoxScript>().isCarbox = true;
+        num.GetComponent<BoxScript>().CoinPrefab = CoinPrefab;
+        moverBoxScript.boxNumber++;
+        rackBoxesList[boxCountnum-1].SetActive(false);
+        if (boxCountnum > 0)
+        {
+            boxCountnum--;
+        }       
+        if (isMoverInArea)
+        {
+            StartCoroutine(MoverStay());
+        }
+    }
+}
